c543604a88b798d882279c098f9790ce2731e5a5
Kyu Lee <kwooler@gmail.com>
2023-04-28 18:00:28 -0700

    CS1M2Q1

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: clean test
-programtest: tests.cpp Q1.cpp 
-	g++ -fsanitize=address --std=c++17 tests.cpp Q1.cpp -o programtest 
+programtest: tests.cpp 
+	g++ -fsanitize=address --std=c++17 tests.cpp -o programtest 
 test: programtest 
 	./programtest
 clean:
--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#include "Q1.hpp"
+#include "main.hpp"
 
 int main()
 {
         string filename;
         int lnum, wnum;
         char mfu;
+
+        filename = "t0.dat";
+        lnum = getNumLines(filename);
+        cout << "The number of lines: " << lnum << endl;
+        lnum = getNumWords(filename);
+        cout << "The number of words: " << lnum << endl;
+        mfu = getMFU(filename);
+        cout << "the most frequently used character: " << mfu << endl;
 
         filename = "textfile0.dat";
         lnum = getNumLines(filename);
